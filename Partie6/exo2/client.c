04f33f931962185ae5d14acc8c316d713d94aab7
Florian Chevalier <gnvs4c28t8@privaterelay.appleid.com>
2025-12-02 15:00:46 +0100

    Ajout exo2 - Partie 6

---
+++ Partie6/exo2/client.c
@@ -0,0 +1,77 @@
+#define _GNU_SOURCE
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define MAX_BUFFER 1024
+#define ADRESSE "127.0.0.1"
+
+void lireMessage(char tampon[]) {
+    printf("Saisir un message à envoyer :\n");
+    fgets(tampon, MAX_BUFFER, stdin);
+    strtok(tampon, "\n");
+}
+
+int main() {
+    char message[MAX_BUFFER];
+
+    int fdSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (fdSocket < 0) {
+        printf("socket incorrecte\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // 2. Configuration de l'adresse du serveur
+    struct sockaddr_in coordonneesServeur = {0};
+    coordonneesServeur.sin_family = AF_INET; // de type TCP
+    coordonneesServeur.sin_port = htons(PORT); // le port d'écoute du serveur
+    inet_aton(ADRESSE, &coordonneesServeur.sin_addr); // adresse du serveur
+
+    // 3. Connexion au serveur
+    printf("[Client TCP] Connexion au serveur %s:%d...\n", ADRESSE, PORT);
+    if (connect(fdSocket, (struct sockaddr*) &coordonneesServeur, sizeof coordonneesServeur) == -1) {
+        perror("connect");
+        close(fdSocket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("[Client TCP] Connecté !\n");
+    printf("[Client] Entrez vos messages (exit pour quitter) :\n");
+
+    while (1) {
+        lireMessage(message);
+
+        printf("\033[1A"); // remonte d’une ligne
+        printf("\033[2K"); // efface la ligne
+
+        // on envoie le message au serveur
+        send(fdSocket, message, strlen(message), 0);
+
+        // on attend la réponse du serveur
+        int nbRecu = recv(fdSocket, message, MAX_BUFFER, 0);
+
+        if (nbRecu == 0) {
+            printf("[Client] Serveur déconnecté\n");
+            break;
+        }
+
+        if (nbRecu > 0) {
+            message[nbRecu] = 0;
+            printf("[Client] Recu : %s\n", message);
+        }
+    }
+
+    printf("\n[Client] Fermeture de la connexion...\n");
+    shutdown(fdSocket, SHUT_WR); // On n'envoie plus
+
+    close(fdSocket);
+
+    return EXIT_SUCCESS;
+}
---
+++ Partie6/exo2/serveur.c
@@ -0,0 +1,109 @@
+#define _GNU_SOURCE
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define MAX_BUFFER 1024
+#define BACKLOG 5
+#define MAX_CLIENTS 10
+#define EXIT "exit"
+
+
+void lireMessage(char tampon[]) {
+    printf("Saisir un message à envoyer :\n");
+    fgets(tampon, MAX_BUFFER, stdin);
+    strtok(tampon, "\n");
+}
+
+int testQuitter(char tampon[]) {
+    return strcmp(tampon, EXIT) == 0;
+}
+
+
+int main() {
+    char message[MAX_BUFFER];
+
+    // 1. connexion TCP en mode connecté
+    int socketAttente = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketAttente == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Option SO_REUSEADDR pour éviter "Address already in use", uniquement côté serveur
+    int opt = 1;
+    if (setsockopt(socketAttente, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof opt) == -1) {
+        perror("setsockopt");
+        close(socketAttente);
+        exit(EXIT_FAILURE);
+    }
+
+    // 2. Configuration de l'adresse (écoute sur toutes les interfaces, port 8080)
+    struct sockaddr_in coordonneesServeur = {0};
+    coordonneesServeur.sin_family = AF_INET;
+    coordonneesServeur.sin_port = htons(PORT); // port 8080
+    coordonneesServeur.sin_addr.s_addr = htonl(INADDR_ANY); // 0.0.0.0 → écouter sur toutes les interfaces IPv4
+
+    // 3. Bind
+    if (bind(socketAttente, (struct sockaddr*) &coordonneesServeur, sizeof coordonneesServeur) == -1) {
+        perror("bind");
+        close(socketAttente);
+        exit(EXIT_FAILURE);
+    }
+
+    // 5. Listen
+    if (listen(socketAttente, BACKLOG) == -1) {
+        perror("listen");
+        close(socketAttente);
+        exit(EXIT_FAILURE);
+    }
+
+    // 6. Acceptation d'un client (à faire dans une boucle pour en accepter plusieurs)
+    struct sockaddr_in coordonneesClient;
+    socklen_t tailleCoord = sizeof coordonneesClient;
+
+    int socketCommunication = accept(socketAttente, (struct sockaddr*) &coordonneesClient, &tailleCoord);
+    if (socketCommunication == -1) {
+        perror("accept");
+        close(socketAttente);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client connecté - %s:%d\n",
+               inet_ntoa(coordonneesClient.sin_addr),
+               ntohs(coordonneesClient.sin_port));
+
+    while (1) {
+        // on attend le message du client
+        // la fonction recv est bloquante
+        int nbRecu = recv(socketCommunication, message, MAX_BUFFER, 0);
+
+        if (nbRecu > 0) {
+            message[nbRecu] = 0;
+            printf("[Serveur] Reçu : %s\n", message);
+
+            if (testQuitter(message)) {
+                printf("[Serveur] Client déconnecté\n");
+                break; // on quitte la boucle
+            }
+        }
+
+        // on envoie le message au client
+        send(socketCommunication, message, strlen(message), 0);
+        printf("[Serveur] Écho renvoyé\n");
+    }
+
+    printf("\n[Serveur] Fermeture de la connexion...\n");
+    shutdown(socketCommunication, SHUT_RDWR); // On n'envoie plus
+
+    close(socketCommunication);
+    close(socketAttente);
+
+    return EXIT_SUCCESS;
+}
